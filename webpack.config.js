0993282b74a123abe107573c7839ae4f088c9ebe
Ahmed Ibrahim <ib153507@gmail.com>
2022-09-17 06:27:41 +0200

    Update .gitignore

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: "./src/server.ts",
+  output: {
+    filename: "./dist/bundle.js",
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      { test: /\.js$/, loader: "source-map-loader" },
+    ],
+  },
+};
