6a07a365b9f3518bda42273f4cc1892621715636
Ahmed Ibrahim <ib153507@gmail.com>
2022-09-17 07:00:24 +0200

    Add bash script to update the file extension

---
+++ tots.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curr_files=$( ls ./src/*/*.js )
+
+for curr_file in $curr_files; do
+    file_name=$( echo "$curr_file" | cut -d '.' -f 2 )
+    new_file=".$file_name.ts"
+    echo "$curr_file -> $new_file"
+    mv "$curr_file" "$new_file"
+done
