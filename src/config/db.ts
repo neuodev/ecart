66b8c87ec0351b21b9f7bf8044cd5677d24fa01b
Ahmed Ibrahim <ib153507@gmail.com>
2022-09-18 05:45:32 +0200

    Update docker.yml

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,9 +3,14 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     let uri = process.env.MONGO_URI;
+
     if (!uri) {
-      throw new Error("Missing mongo URI");
+      throw new Error(
+        "Missing mongo URI\nTry to use docker `docker-compose up -d` then add this `MONGO_URI=mongodb://localhost/wallet` to your `.env`"
+      );
     }
+
+    console.log(`[uri] ${uri}`);
 
     const conn = await mongoose.connect(uri);
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
