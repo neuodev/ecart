662f3a390416d8013576629689bc4ec74113a71b
Ahmed Ibrahim <ib153507@gmail.com>
2022-09-20 07:40:21 +0200

    Migrate all files to be txs

--- frontend/src/utils/cost.ts
+++
@@ -1,8 +0,0 @@
-export const calcTotal = (discount, price, qty) => {
-  discount = Number(discount);
-  price = Number(price);
-  qty = Number(qty);
-  const itemPrice = price - price * (discount / 100);
-  const itemsPrice = qty * itemPrice;
-  return Number(itemsPrice);
-};
