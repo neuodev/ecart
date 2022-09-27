cd2784814eb35fc28be3fa34b234221247a71ca6
Ahmed Ibrahim <ib153507@gmail.com>
2022-09-27 05:44:34 +0200

    Revamp information page

--- client/src/components/checkouts/ShippingForm.js
+++ client/src/components/checkouts/ShippingForm.js
@@ -1,7 +1,15 @@
-import { Breadcrumbs, Checkbox } from "@mui/material";
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import React from "react";
-import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { BsChevronCompactLeft } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import countries from "../../utils/countries";
 import { useSelector, useDispatch } from "react-redux";
@@ -14,6 +22,22 @@ const ShippingForm = () => {
   const navigate = useNavigate();
 
   const { shippingAddress } = useSelector((state) => state.cart);
+  const [state, setState] = useState({
+    email: shippingAddress.email || "",
+    firstName: shippingAddress.firstName || "",
+    lastName: shippingAddress.lastName || "",
+    address: shippingAddress.address || "",
+    city: shippingAddress.city || "",
+    postalCode: shippingAddress.postalCode || "",
+    country: shippingAddress.country
+      ? shippingAddress.country
+      : countries[1].name,
+    apartment: shippingAddress.apartment || "",
+  });
+
+  const [save, setSave] = useState(shippingAddress.save);
+  const [openCountries, setOpenCountries] = useState(false);
+  // Todo: Remove these states
   const [email, setEmail] = useState(shippingAddress.email || "");
   const [firstName, setFirstName] = useState(shippingAddress.firstName);
   const [lastName, setLastName] = useState(shippingAddress.lastName);
@@ -24,9 +48,14 @@ const ShippingForm = () => {
     shippingAddress.country ? shippingAddress.country : countries[1].name
   );
   const [apartment, setApartment] = useState(shippingAddress.apartment);
-  const [save, setSave] = useState(shippingAddress.save);
-  const [openCountries, setOpenCountries] = useState(false);
   const [alert, setAlert] = useState("");
+
+  const stateHandler = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const submitHandler = () => {
     if (
@@ -69,167 +98,174 @@ const ShippingForm = () => {
         <CheckoutSteps currStep={2} />
       </div>
       <div>
-        <div className="py-4">
+        <div>
           {alert && (
             <div className="py-4 font-bold uppercase tracking-wider bg-red-200 text-red-800 rounded-md  text-center mb-4">
               <h1>{alert}</h1>
             </div>
           )}
           <div>
-            <h1 className="text-gray-700 text-lg mb-2">Contact Information</h1>
-            <div className="flex-col flex ">
-              <label htmlFor="Email" className="mb-1 font-medium text-sm  ">
-                Email
-              </label>
-              <input
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="border py-2 pl-4 focus:outline-none focus:ring-1 focus:ring-green-400 rounded-md w-full bg-gray-50 text-gray-700"
+            <h1 className="text-gray-700 text-2xl font-bold mb-5">
+              Contact Information
+            </h1>
+            <div className="flex-col flex">
+              <TextField
+                onChange={stateHandler}
+                value={state.email}
+                label="Email"
+                name="email"
+                placeholder="Your email address"
+                size="small"
               />
             </div>
           </div>
-          <div className="py-4 ">
-            <h1 className="text-lg text-gray-700 mb-3">Shipping address </h1>
-            <div className="md:flex  items-center md:space-x-2">
-              <div className="mb-3 flex-col flex  w-full">
-                <label
-                  htmlFor="first name "
-                  className="mb-1 font-medium text-sm  "
-                >
-                  First Name
-                </label>
-                <input
-                  type="text"
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                  className="border py-2 pl-4 focus:outline-none focus:ring-1 focus:ring-green-400 rounded-md w-full bg-gray-50 text-gray-700"
-                />
-              </div>
-              <div className="mb-3 flex-col flex w-full">
-                <label
-                  htmlFor="first name "
-                  className="mb-1 font-medium text-sm  "
-                >
-                  Last Name
-                </label>
-                <input
-                  type="text"
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
-                  className="border py-2 pl-4 focus:outline-none focus:ring-1 focus:ring-green-400 rounded-md w-full bg-gray-50 text-gray-700"
-                />
-              </div>
+          <div className="py-4">
+            <h1 className="text-gray-700 text-2xl font-bold mb-5">
+              Shipping address
+            </h1>
+            <div className="md:flex items-center md:space-x-2 mb-5">
+              <TextField
+                onChange={stateHandler}
+                fullWidth
+                value={state.firstName}
+                label="First name"
+                name="firstName"
+                placeholder="Your first name"
+                size="small"
+              />
+              <TextField
+                onChange={stateHandler}
+                value={state.lastName}
+                fullWidth
+                label="Last name"
+                name="lastName"
+                placeholder="Your last name"
+                size="small"
+              />
             </div>
-            <div className="mb-3 flex-col flex w-full">
-              <label
-                htmlFor="first name "
-                className="mb-1 font-medium text-sm  "
-              >
+            <div className="mb-5 flex-col flex w-full">
+              <TextField
+                onChange={stateHandler}
+                value={state.address}
+                fullWidth
+                label="Address"
+                name="address"
+                placeholder="Enter your address"
+                size="small"
+              />
+              {/* <label htmlFor="address" className="mb-1 font-medium text-sm  ">
                 Address
               </label>
               <input
+                name="address"
                 type="text"
                 value={address}
                 onChange={(e) => setAddress(e.target.value)}
                 className="border py-2 pl-4 focus:outline-none focus:ring-1 focus:ring-green-400 rounded-md w-full bg-gray-50 text-gray-700"
-              />
+              /> */}
             </div>
-            <div className="mb-3 flex-col flex w-full">
-              <label
-                htmlFor="first name "
-                className="mb-1 font-medium text-sm  "
-              >
+            <div className="mb-5 flex-col flex w-full">
+              <TextField
+                onChange={stateHandler}
+                value={state.address}
+                fullWidth
+                label="Apartment, suite, etc"
+                name="apartment"
+                placeholder="Enter your apartment"
+                size="small"
+              />
+              {/* <label htmlFor="apartment" className="mb-1 font-medium text-sm  ">
                 Apartment, suite, etc.
               </label>
               <input
+                name="apartment"
                 type="text"
                 value={apartment}
                 onChange={(e) => setApartment(e.target.value)}
                 className="border  py-2 pl-4 focus:outline-none focus:ring-1 focus:ring-green-400 rounded-md w-full bg-gray-50 text-gray-700"
-              />
+              /> */}
             </div>
             <div className="md:flex items-center md:space-x-2">
-              <div className="mb-3 flex-col flex w-full">
-                <label
-                  htmlFor="first name "
-                  className="mb-1 font-medium text-sm  "
-                >
-                  City
-                </label>
-                <input
-                  type="text"
-                  value={city}
-                  onChange={(e) => setCity(e.target.value)}
-                  className="border py-2 pl-4 focus:outline-none focus:ring-1 focus:ring-green-400 rounded-md w-full bg-gray-50 text-gray-700"
-                />
-              </div>
-              <div className="mb-3 flex-col flex w-full">
-                <label
-                  htmlFor="first name "
-                  className="mb-1 font-medium text-sm  "
-                >
-                  Postal Code
-                </label>
-                <input
-                  type="text"
-                  value={postalCode}
-                  onChange={(e) => setPostalCode(e.target.value)}
-                  className="border py-2 pl-4 focus:outline-none focus:ring-1 focus:ring-green-400 rounded-md w-full bg-gray-50 text-gray-700"
-                />
-              </div>
+              <TextField
+                onChange={stateHandler}
+                value={state.city}
+                fullWidth
+                label="City"
+                name="city"
+                placeholder="Enter your city"
+                size="small"
+              />
+
+              <TextField
+                onChange={stateHandler}
+                value={state.postalCode}
+                fullWidth
+                label="Postal code"
+                name="postalCode"
+                placeholder="Enter your postal code"
+                size="small"
+              />
             </div>
           </div>
-          <div
-            onMouseEnter={() => setOpenCountries(true)}
-            onMouseLeave={() => setOpenCountries(false)}
-            className="mb-3 flex-col flex w-full rounded-md overflow-hidden  "
-          >
-            <label htmlFor="first name " className="mb-1 font-medium text-sm  ">
-              Country/Region
-            </label>
-            <div className=" py-3 px-3 bg-green-200 rounded-t-md  text-green-900 font-bold text-lg  ">
-              {country}
-            </div>
-            <div
-              className={`bg-gray-100 overflow-y-scroll py-2 ${
-                !openCountries ? "h-0 opacity-0  " : "block   opacity-100 h-64"
-              }  transition-all duration-500 `}
-            >
-              <div className="">
+          <div className="mb-5">
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">
+                Country/Region
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={state.country}
+                name="country"
+                size="small"
+                label="Country/Region"
+                onChange={stateHandler}
+              >
+                {/* <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem> */}
                 {countries.map((country) => (
-                  <div
-                    onClick={() => chooseCountry(country.name)}
-                    className="py-1 px-3 hover:bg-gray-200  cursor-pointer font-medium text-sm"
-                  >
-                    {country.name}
-                  </div>
+                  <MenuItem value={country.name} dense>
+                    {country.name} ({country.code})
+                  </MenuItem>
                 ))}
-              </div>
-            </div>
+              </Select>
+            </FormControl>
           </div>
         </div>
-        <div className="-mt-4 mb-4 flex items-center  space-x-0">
+        <div className="-ml-3 mb-5 flex items-center  space-x-0">
           <Checkbox
             checked={save ? true : false}
             onChange={() => setSave(!save)}
             color="default"
           />{" "}
           <span className="-ml-1 text-sm font-light">
-            Save this information for next time{" "}
+            Save your info for the next time
           </span>
         </div>
 
-        <button
+        <Button
           onClick={submitHandler}
-          className="block w-full bg-green-400 rounded-sm shadow-md  text-center  py-3 px-4  text-white font-semibold  hover:bg-green-300 transition-colors duration-300 focus:ring-1 focus:ring-green-500  mb-4"
+          variant="dark"
+          fullWidth
+          sx={{ mb: "16px" }}
         >
           Continue to shipping
-        </button>
-        <Link className=" flex items-center justify-center w-full border rounded-sm shadow-md  text-center  py-3 px-4  text-green-500 font-semibold   focus:ring-1 focus:ring-green-500 mb-4 ">
-          <BsChevronCompactLeft />
+        </Button>
+        <Button
+          LinkComponent={Link}
+          to="/cart/1"
+          startIcon={<BsChevronCompactLeft />}
+          onClick={submitHandler}
+          variant="dark-outlined"
+          size="small"
+          sx={{
+            padding: "12px 20px",
+          }}
+          fullWidth
+        >
           Return to cart
-        </Link>
+        </Button>
       </div>
     </div>
   );
