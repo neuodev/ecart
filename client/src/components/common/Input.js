09cb3d3187a21922381aefeea9d68263e78c9a89
Ahmed Ibrahim <ib153507@gmail.com>
2022-09-29 08:24:00 +0200

    Add eye button to toggle the password field

--- client/src/components/common/Input.js
+++ client/src/components/common/Input.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import { IconButton } from "@mui/material";
 
-const Input = ({ label, value, onChange, error, name, placeholder }) => {
+const Input = ({ label, value, onChange, error, name, placeholder, type }) => {
+  const [show, setShow] = useState(false);
+
   return (
     <div>
       <p
@@ -8,16 +13,25 @@ const Input = ({ label, value, onChange, error, name, placeholder }) => {
       >
         {label}
       </p>
-      <input
-        value={value}
-        name={name}
-        onChange={onChange}
-        className={`w-full py-2 px-4 rounded-sm focus:outline-none focus:ring-1 border ${
-          error ? "border-red-500 focus:ring-red-400" : "focus:ring-gray-400"
-        }`}
-        type="text"
-        placeholder={placeholder}
-      />
+      <div className="relative bg-yellow-100 border">
+        <input
+          value={value}
+          name={name}
+          onChange={onChange}
+          className={`w-full py-2 px-4 rounded-sm focus:outline-none focus:ring-1 border ${
+            error ? "border-red-500 focus:ring-red-400" : "focus:ring-gray-400"
+          }`}
+          type={show === true ? "text" : type || "text"}
+          placeholder={placeholder}
+        />
+        {type === "password" && (
+          <div className="absolute top-1/2 right-2 -translate-y-1/2  z-10">
+            <IconButton onClick={() => setShow(!show)}>
+              {!show ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
+            </IconButton>
+          </div>
+        )}
+      </div>
       <span className="block w-full h-2 mt-1 text-xs text-red-500">
         {error}
       </span>
