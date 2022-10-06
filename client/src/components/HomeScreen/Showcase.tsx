a0f44f469fdbac8ac56aa608f0f057ff17197320
Ahmed Ibrahim <ib153507@gmail.com>
2022-10-06 09:24:37 +0200

    Add autoplay to the showcase slider

--- client/src/components/HomeScreen/Showcase.tsx
+++ client/src/components/HomeScreen/Showcase.tsx
@@ -12,10 +12,10 @@ const Showcase = () => {
       <Swiper
         loop
         className="bg-gray-900"
-        // autoplay={{
-        //   delay: 2000,
-        //   disableOnInteraction: false,
-        // }}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
         modules={[Autoplay]}
       >
         {slides.map((slide, idx) => (
